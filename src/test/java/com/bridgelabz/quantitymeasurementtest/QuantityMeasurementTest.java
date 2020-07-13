d0d2291c3af64cd3712f7671506cddc83c4104b0
“Aniket_D” <aniketdakhare@gmail.com>
2020-07-14 04:12:50 +0530

    MODIFIED Test cases after the merge conflict.

--- src/test/java/com/bridgelabz/quantitymeasurementtest/QuantityMeasurementTest.java
+++ src/test/java/com/bridgelabz/quantitymeasurementtest/QuantityMeasurementTest.java
@@ -8,11 +8,92 @@ import org.junit.Test;
 public class QuantityMeasurementTest
 {
     @Test
-    public void givenLengthsInFeetAndInch_WhenCompared_ShouldReturnTrue()
+    public void givenLengthsInFeet_WhenEqual_ShouldReturnTrue()
     {
-        QuantityMeasurement quantityMeasurement = new QuantityMeasurement();
-        Unit[] unitTypes = {Unit.INCH, Unit.FEET};
-        boolean result = quantityMeasurement.compareLengths(unitTypes, 12.0, 1.0);
+        QuantityMeasurement length1 = new QuantityMeasurement(0.0, Unit.FEET);
+        QuantityMeasurement length2 = new QuantityMeasurement(0.0, Unit.FEET);
+        boolean result = length1.equals(length2);
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void givenLengthsInFeet_WhenOneOfTheLengthIsNull_ShouldReturnFalse()
+    {
+        QuantityMeasurement length1 = new QuantityMeasurement(0.0, Unit.FEET);
+        QuantityMeasurement length2 = null;
+        boolean result = length1.equals(length2);
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void givenLengthsInFeet_WhenCheckedForTheReference_ShouldReturnFalse()
+    {
+        QuantityMeasurement length1 = new QuantityMeasurement(0.0, Unit.FEET);
+        QuantityMeasurement length2 = new QuantityMeasurement();
+        boolean result = length1.equals(length2);
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void givenLengthsInFeet_WhenCheckedForTheType_ShouldReturnFalse()
+    {
+        QuantityMeasurement length1 = new QuantityMeasurement(0.0, Unit.FEET);
+        Object length2 = new Object();
+        boolean result = length1.equals(length2);
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void givenLengthsInFeet_WhenCheckedForValue_ShouldReturnTrue()
+    {
+        QuantityMeasurement length1 = new QuantityMeasurement(5.0, Unit.FEET);
+        QuantityMeasurement length2 = new QuantityMeasurement(5.0, Unit.FEET);
+        boolean result = length1.equals(length2);
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void givenLengthsInInch_WhenEqual_ShouldReturnTrue()
+    {
+        QuantityMeasurement length1 = new QuantityMeasurement(0.0, Unit.INCH);
+        QuantityMeasurement length2 = new QuantityMeasurement(0.0, Unit.INCH);
+        boolean result = length1.equals(length2);
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void givenLengthsInInch_WhenOneOfTheLengthIsNull_ShouldReturnFalse()
+    {
+        QuantityMeasurement length1 = new QuantityMeasurement(0.0, Unit.INCH);
+        QuantityMeasurement length2 = null;
+        boolean result = length1.equals(length2);
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void givenLengthsInInch_WhenCheckedForTheReference_ShouldReturnFalse()
+    {
+        QuantityMeasurement length1 = new QuantityMeasurement(0.0, Unit.INCH);
+        QuantityMeasurement length2 = new QuantityMeasurement();
+        boolean result = length1.equals(length2);
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void givenLengthsInInch_WhenCheckedForTheType_ShouldReturnFalse()
+    {
+        QuantityMeasurement length1 = new QuantityMeasurement(0.0, Unit.INCH);
+        Object length2 = new Object();
+        boolean result = length1.equals(length2);
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void givenLengthsInInch_WhenCheckedForValue_ShouldReturnTrue()
+    {
+        QuantityMeasurement length1 = new QuantityMeasurement(5.0, Unit.INCH);
+        QuantityMeasurement length2 = new QuantityMeasurement(5.0, Unit.INCH);
+        boolean result = length1.equals(length2);
         Assert.assertTrue(result);
     }
 }
