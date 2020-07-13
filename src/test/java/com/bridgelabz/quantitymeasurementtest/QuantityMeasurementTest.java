34f3cfff427ea10ba4eb9f36f6a939cf4098109e
“Aniket_D” <aniketdakhare@gmail.com>
2020-07-14 01:43:57 +0530

    ADDED Test case to get false value checked for the reference.

--- src/test/java/com/bridgelabz/quantitymeasurementtest/QuantityMeasurementTest.java
+++ src/test/java/com/bridgelabz/quantitymeasurementtest/QuantityMeasurementTest.java
@@ -20,8 +20,19 @@ public class QuantityMeasurementTest
     public void givenLengthsInFeet_WhenOneOfTheLengthIsNull_ShouldReturnFalse()
     {
         QuantityMeasurement length1 = new QuantityMeasurement(0.0, Unit.FEET);
+        QuantityMeasurement length2 = null;
+        boolean result = length1.equals(length2);
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void givenLengthsInFeet_WhenCheckedForTheReference_ShouldReturnFalse()
+    {
+        QuantityMeasurement length1 = new QuantityMeasurement(0.0, Unit.FEET);
         QuantityMeasurement length2 = new QuantityMeasurement();
         boolean result = length1.equals(length2);
         Assert.assertFalse(result);
     }
+
+
 }
