7306951cbff8c06fb4a030f9bfecf9551366f28c
“Aniket_D” <aniketdakhare@gmail.com>
2020-07-14 00:13:42 +0530

    REFACTORED Test case to get true value if the given lengths in Feet are equal by using TDD approach.

--- src/test/java/com/bridgelabz/quantitymeasurementtest/QuantityMeasurementTest.java
+++ src/test/java/com/bridgelabz/quantitymeasurementtest/QuantityMeasurementTest.java
@@ -8,11 +8,12 @@ import org.junit.Test;
 public class QuantityMeasurementTest
 {
     @Test
-    public void givenLengthsInFeetAndInch_WhenCompared_ShouldReturnTrue()
+    public void givenLengthsInFeet_WhenEqual_ShouldReturnTrue()
     {
         QuantityMeasurement quantityMeasurement = new QuantityMeasurement();
-        Unit[] unitTypes = {Unit.INCH, Unit.FEET};
-        boolean result = quantityMeasurement.compareLengths(unitTypes, 12.0, 1.0);
+        Unit[] unitTypes = {Unit.FEET, Unit.FEET};
+        boolean result = quantityMeasurement.compareLengths(unitTypes, 1.0, 1.0);
         Assert.assertTrue(result);
     }
+
 }
