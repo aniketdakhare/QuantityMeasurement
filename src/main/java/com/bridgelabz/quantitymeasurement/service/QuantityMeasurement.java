edcf2ff38d21f196056ff2a0376681e3f26b5d65
“Aniket_D” <aniketdakhare@gmail.com>
2020-07-14 00:14:12 +0530

    REFACTORED code to return true value if the given lengths in Feet are equal by using TDD approach.

--- src/main/java/com/bridgelabz/quantitymeasurement/service/QuantityMeasurement.java
+++ src/main/java/com/bridgelabz/quantitymeasurement/service/QuantityMeasurement.java
@@ -10,10 +10,10 @@ public class QuantityMeasurement
      * @param length provides corresponding lengths
      * @return result after comparing the values
      */
-    public boolean compareLengths(Unit[] units,double... length)
+    public boolean compareLengths(Unit[] units, double... length)
     {
-        double value1 = (length[0] * units[0].returnValue());
-        double value2 = (length[1] * units[1].returnValue());
+        double value1 = (length[0] * units[0].value);
+        double value2 = (length[1] * units[1].value);
         return Double.compare(value1, value2) == 0;
     }
 }
