5d50994fecf2fcaf546333343c279b70e241b5ea
“Aniket_D” <aniketdakhare@gmail.com>
2020-07-16 03:58:27 +0530

    ADDED Enum constants for weight.

--- src/main/java/com/bridgelabz/quantitymeasurement/utility/Unit.java
+++ src/main/java/com/bridgelabz/quantitymeasurement/utility/Unit.java
@@ -6,18 +6,20 @@ public enum Unit
     INCH(1, QuantityType.LENGTH), FEET(12, QuantityType.LENGTH),
     YARD(36, QuantityType.LENGTH), CENTIMETER(0.4, QuantityType.LENGTH),
     GALLON(3.78, QuantityType.VOLUME), LITRE(1.0, QuantityType.VOLUME),
-    MILLILITRE(0.001,QuantityType.VOLUME);
+    MILLILITRE(0.001,QuantityType.VOLUME), KILOGRAM(1.0, QuantityType.WEIGHT),
+    GRAM(0.001, QuantityType.WEIGHT), TONNE(1000.0, QuantityType.WEIGHT);
 
     public enum QuantityType
     {
-        LENGTH, VOLUME
+        LENGTH, VOLUME, WEIGHT
     }
 
     public final double unitValue;
     public final QuantityType quantityType;
 
 
-    Unit(double unitValue, QuantityType quantityType) {
+    Unit(double unitValue, QuantityType quantityType)
+    {
         this.unitValue = unitValue;
         this.quantityType = quantityType;
     }
