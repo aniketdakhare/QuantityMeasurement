8508e08a77aa3bdf596a1e943ec56e586d50e757
“Aniket_D” <aniketdakhare@gmail.com>
2020-07-16 02:52:53 +0530

    REFACTORED Code by adding enum for Quantity type.

--- src/main/java/com/bridgelabz/quantitymeasurement/utility/Unit.java
+++ src/main/java/com/bridgelabz/quantitymeasurement/utility/Unit.java
@@ -3,12 +3,22 @@ package com.bridgelabz.quantitymeasurement.utility;
 public enum Unit
 {
 
-    INCH(1), FEET(12), YARD(36), CENTIMETER(0.4);
+    INCH(1, QuantityType.LENGTH), FEET(12, QuantityType.LENGTH),
+    YARD(36, QuantityType.LENGTH), CENTIMETER(0.4, QuantityType.LENGTH),
+    GALLON(3.78, QuantityType.VOLUME), LITRE(1.0, QuantityType.VOLUME),
+    MILLILITRE(0.001,QuantityType.VOLUME);
+
+    public enum QuantityType
+    {
+        LENGTH, VOLUME
+    }
 
     public final double unitValue;
+    public final QuantityType quantityType;
 
-    Unit(double unitValue)
-    {
+
+    Unit(double unitValue, QuantityType quantityType) {
         this.unitValue = unitValue;
+        this.quantityType = quantityType;
     }
 }
